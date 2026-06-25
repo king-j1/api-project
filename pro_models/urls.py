b24b27edebf92de38cd49eb36db2fe975d7a6d82
kingj1 <agallie962@gmail.com>
2026-06-25 12:51:42 -0500

    debug 404

--- pro_models/urls.py
+++ pro_models/urls.py
@@ -1,12 +1,20 @@
+
+
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
 
-# Serve media files even when DEBUG=False on Railway
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Force serve media files on Railway even with DEBUG=False
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
