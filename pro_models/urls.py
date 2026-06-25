940e920c2337ce47fea97f2ac12a950f20e2b4f3
kingj1 <agallie962@gmail.com>
2026-06-25 12:33:06 -0500

    fixing 404

--- pro_models/urls.py
+++ pro_models/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static # Import static helper tools
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
-    
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Serve media files even when DEBUG=False on Railway
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
