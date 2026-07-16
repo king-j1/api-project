25a5a95d80e887dd3fa8363022014723edbb9473
kingj1 <agallie962@gmail.com>
2026-07-16 09:22:29 -0500

    Allow current Vercel frontend origin

--- pro_models/settings.py
+++ pro_models/settings.py
@@ -162,6 +162,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5174", # <-- your React port
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "https://photo-gallery-delta-lilac.vercel.app",
     "https://photo-gallery-rpve.vercel.app",
 ]
 
